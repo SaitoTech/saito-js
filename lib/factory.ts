6cb52b034f7c0b370920c8a37e4c0c47de00337e
Sanka Darshana <sankadarshana@gmail.com>
2023-04-25 10:40:59 +0530

    wallet related changes

--- lib/factory.ts
+++ lib/factory.ts
@@ -2,24 +2,31 @@ import Block from "./block";
 import Transaction from "./transaction";
 import Slip from "./slip";
 import Peer from "./peer";
+import Wallet from "./wallet";
+import Blockchain from "./blockchain";
 
 export default class Factory {
-    constructor() {
-    }
+  constructor() {}
 
-    public createBlock(data?: any): Block {
-        return new Block(data);
-    }
+  public createBlock(data?: any): Block {
+    return new Block(data);
+  }
 
-    public createTransaction<T extends Transaction>(data?: any): Transaction {
-        return new Transaction(data);
-    }
+  public createTransaction<T extends Transaction>(data?: any): Transaction {
+    return new Transaction(data);
+  }
 
-    public createSlip(data?: any): Slip {
-        return new Slip(data);
-    }
+  public createSlip(data?: any): Slip {
+    return new Slip(data);
+  }
 
-    public createPeer(data?: any): Peer {
-        return new Peer(data);
-    }
+  public createPeer(data?: any): Peer {
+    return new Peer(data);
+  }
+  public createWallet(data: any): Wallet {
+    return new Wallet(data);
+  }
+  public createBlockchain(data: any): Blockchain {
+    return new Blockchain(data);
+  }
 }
--- lib/wallet.ts
+++ lib/wallet.ts
@@ -1,40 +1,61 @@
-import type {WasmWallet} from "saito-wasm/dist/types/pkg/node/index_bg";
+import type { WasmWallet } from "saito-wasm/dist/types/pkg/node/index_bg";
 import Saito from "../saito";
 
+export const DefaultEmptyPrivateKey =
+  "0000000000000000000000000000000000000000000000000000000000000000";
+export const DefaultEmptyPublicKey =
+  "000000000000000000000000000000000000000000000000000000000000000000";
+
 export default class Wallet {
-    protected wallet: WasmWallet;
-    public static Type: any;
+  protected wallet: WasmWallet;
+  public static Type: any;
 
-    constructor(wallet: WasmWallet) {
-        this.wallet = wallet;
-    }
+  constructor(wallet: WasmWallet) {
+    this.wallet = wallet;
+  }
 
-    public async save() {
-        return this.wallet.save();
-    }
+  public async save() {
+    return this.wallet.save();
+  }
 
-    public async load() {
-        return this.wallet.load();
-    }
+  public async load() {
+    return this.wallet.load();
+  }
 
-    public async reset() {
-        return this.wallet.reset();
-    }
+  public async reset() {
+    return this.wallet.reset();
+  }
 
-    public async getPublicKey() {
-        return this.wallet.get_public_key();
-    }
+  public async getPublicKey() {
+    let key = await this.wallet.get_public_key();
+    return key === DefaultEmptyPublicKey ? "" : key;
+  }
 
-    public async getPrivateKey() {
-        return this.wallet.get_private_key();
+  public async setPublicKey(key: string) {
+    if (key === "") {
+      key = DefaultEmptyPublicKey;
     }
+    return this.wallet.set_public_key(key);
+  }
 
-    public async getBalance() {
-        return this.wallet.get_balance();
-    }
+  public async getPrivateKey() {
+    let key = await this.wallet.get_private_key();
+    return key === DefaultEmptyPrivateKey ? "" : key;
+  }
 
-    public async getPendingTxs() {
-        let txs = await this.wallet.get_pending_txs();
-        return txs.map((tx: any) => Saito.getInstance().factory.createTransaction(tx));
+  public async setPrivateKey(key: string) {
+    if (key === "") {
+      key = DefaultEmptyPrivateKey;
     }
+    return this.wallet.set_private_key(key);
+  }
+
+  public async getBalance() {
+    return this.wallet.get_balance();
+  }
+
+  public async getPendingTxs() {
+    let txs = await this.wallet.get_pending_txs();
+    return txs.map((tx: any) => Saito.getInstance().factory.createTransaction(tx));
+  }
 }
